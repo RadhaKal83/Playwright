d0723754c95b3194fe39683c907e48d86b57955e
Radha K <radhakalidass@gmail.com>
2025-03-30 16:30:49 +0300

    Add files via upload
    Day 2

---
+++ NumberType.js
@@ -0,0 +1,14 @@
+
+
+
+function numberType(number = 9){
+   
+    if (number>0){
+        console.log("the given number " + number + "is positive");
+    }else if (number<0){
+        console.log(number + "is negative");
+    }else{
+        console.log (number + "is neutral");
+    }
+}
+numberType();
